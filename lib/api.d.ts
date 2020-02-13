fe9e2eea8d8cf48d8c881ce34239060f939feff8
Logan Powell <loganp@tepper.cmu.edu>
2020-02-13 17:45:52 -0500

    syncing supertree with subtree

--- lib/api.d.ts
+++ lib/api.d.ts
@@ -3,45 +3,51 @@ import { IAtom } from "@thi.ng/atom";
 import * as C from "./constants";
 export interface ParsedURL {
     [C.URL_FULL]: string;
-    [C.URL_PATH]: Array<any>;
-    [C.URL_DOMN]: Array<any>;
-    [C.URL_SUBD]: Array<any>;
-    [C.URL_QERY]: Object;
+    [C.URL_PATH]: [];
+    [C.URL_DOMN]: [];
+    [C.URL_SUBD]: [];
+    [C.URL_QERY]: {};
     [C.URL_HASH]: string;
 }
 export interface TargetDOM {
     [C.DOM_NODE]?: HTMLElement;
-    [C.DOM_BODY]?: Object;
-    [C.DOM_HEAD]?: Object;
+    [C.DOM_BODY]?: {};
+    [C.DOM_HEAD]?: {};
 }
-export interface Command {
+export interface ICommand {
     [C.CMD_ARGS]: any;
     [C.CMD_SUB$]?: string;
-    [C.CMD_RESO]?: (acc: Object, res: Object) => any;
-    [C.CMD_ERRO]?: (acc: Object, err: Error) => any;
+    [C.CMD_RESO]?: (acc: {}, res: {}) => any;
+    [C.CMD_ERRO]?: (acc: {}, err: Error) => any;
     [C.CMD_WORK]?: (args: any) => any;
     [C.CMD_SRC$]?: ISubscribable<any>;
 }
+export declare type Command = ICommand | Function;
+export declare type Accumulator = {};
+export declare type HOCommand = (acc: Accumulator) => Command;
+export declare type Task = [Command | HOCommand];
+export declare type HOTask = (acc: Accumulator) => Task;
 export interface BootCFG {
-    [C.CFG_RUTR]: Object | Function;
+    [C.CFG_RUTR]: {} | Function;
     [C.CFG_RUN$]?: any;
     [C.CFG_STOR]?: IAtom<any>;
     [C.CFG_ROOT]?: HTMLElement;
     [C.CFG_VIEW]?: Function;
-    [C.CFG_DRFT]?: Object;
+    [C.CFG_DRFT]?: {};
     [C.CFG_LOG$]?: string;
     [C.CFG_KICK]?: boolean;
 }
 export interface DefaultDraft {
-    [C.$$_PATH]: Array<any>;
+    [C.$$_PATH]: [];
     [C.$$_LOAD]: boolean;
     [C.$$_VIEW]: Function;
     [C.$$_ROOT]: HTMLElement;
-    [C.$$_CMDS]: Object;
+    [C.$$_CMDS]: [];
 }
+export declare type Router = (path: string) => object;
 export interface RouterCFG {
-    [C.ROUTER_PREP]: Command | Array<Command>;
+    [C.ROUTER_PREP]: Command | Task;
     [C.ROUTER_PRFX]: string;
-    [C.ROUTER_POST]: Command | Array<Command>;
-    [C.CFG_RUTR]: Function;
+    [C.ROUTER_POST]: Command | Task;
+    [C.CFG_RUTR]: Router;
 }
--- src/api.ts
+++ src/api.ts
@@ -4,10 +4,10 @@ import * as C from "./constants"
 
 export interface ParsedURL {
   [C.URL_FULL]: string
-  [C.URL_PATH]: Array<any>
-  [C.URL_DOMN]: Array<any>
-  [C.URL_SUBD]: Array<any>
-  [C.URL_QERY]: Object
+  [C.URL_PATH]: []
+  [C.URL_DOMN]: []
+  [C.URL_SUBD]: []
+  [C.URL_QERY]: {}
   [C.URL_HASH]: string
 }
 
@@ -16,49 +16,61 @@ export interface ParsedURL {
  */
 export interface TargetDOM {
   [C.DOM_NODE]?: HTMLElement
-  [C.DOM_BODY]?: Object
-  [C.DOM_HEAD]?: Object
+  [C.DOM_BODY]?: {}
+  [C.DOM_HEAD]?: {}
 }
 /**
  * Command Object.
  *
  * The only required property is `args`
  */
-export interface Command {
+export interface ICommand {
   [C.CMD_ARGS]: any
   [C.CMD_SUB$]?: string
-  [C.CMD_RESO]?: (acc: Object, res: Object) => any
-  [C.CMD_ERRO]?: (acc: Object, err: Error) => any
+  [C.CMD_RESO]?: (acc: {}, res: {}) => any
+  [C.CMD_ERRO]?: (acc: {}, err: Error) => any
   [C.CMD_WORK]?: (args: any) => any
   [C.CMD_SRC$]?: ISubscribable<any>
 }
+
+export type Command = ICommand | Function
+
+export type Accumulator = {}
+
+export type HOCommand = (acc: Accumulator) => Command
+
+export type Task = [Command | HOCommand]
+
+export type HOTask = (acc: Accumulator) => Task
+
 /**
  *
  * Configuration for boot config
  *
  */
 export interface BootCFG {
-  [C.CFG_RUTR]: Object | Function
+  [C.CFG_RUTR]: {} | Function
   [C.CFG_RUN$]?: any
   [C.CFG_STOR]?: IAtom<any>
   [C.CFG_ROOT]?: HTMLElement
   [C.CFG_VIEW]?: Function
-  [C.CFG_DRFT]?: Object
+  [C.CFG_DRFT]?: {}
   [C.CFG_LOG$]?: string
   [C.CFG_KICK]?: boolean
 }
 
 export interface DefaultDraft {
-  [C.$$_PATH]: Array<any>
+  [C.$$_PATH]: []
   [C.$$_LOAD]: boolean
   [C.$$_VIEW]: Function
   [C.$$_ROOT]: HTMLElement
-  [C.$$_CMDS]: Object
+  [C.$$_CMDS]: []
 }
 
+export type Router = (path: string) => object
 export interface RouterCFG {
-  [C.ROUTER_PREP]: Command | Array<Command>
+  [C.ROUTER_PREP]: Command | Task
   [C.ROUTER_PRFX]: string
-  [C.ROUTER_POST]: Command | Array<Command>
-  [C.CFG_RUTR]: Function
+  [C.ROUTER_POST]: Command | Task
+  [C.CFG_RUTR]: Router
 }
