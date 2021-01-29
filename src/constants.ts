2a90afdb9b938c5bad35e5f97ed31d31b87408e1
Logan Powell <loganp@tepper.cmu.edu>
2021-01-29 09:34:47 -0500

    switching to subtree directory and running updates

--- src/constants.ts
+++ src/constants.ts
@@ -2,16 +2,16 @@
  * @module keys
  */
 
-export const URL_FULL = "URL"
-export const URL_PATH = "URL_path"
-export const URL_DATA = "URL_data"
-export const URL_DOMN = "URL_domain"
-export const URL_SUBD = "URL_subdomain"
-export const URL_QERY = "URL_query"
-export const URL_HASH = "URL_hash"
-export const URL_PAGE = "URL_page"
-export const URL_PRSE = "unfurl"
-export const URL_NPRS = "furl"
+export const URL_FULL = 'URL'
+export const URL_PATH = 'URL_path'
+export const URL_DATA = 'URL_data'
+export const URL_DOMN = 'URL_domain'
+export const URL_SUBD = 'URL_subdomain'
+export const URL_QERY = 'URL_query'
+export const URL_HASH = 'URL_hash'
+export const URL_PAGE = 'URL_page'
+export const URL_PRSE = 'unfurl'
+export const URL_NPRS = 'furl'
 
 /**
  * Object Keys for a Parsed URL using `parse` from @-0/utils
@@ -26,38 +26,38 @@ export const URL = {
     HASH: URL_HASH,
     PAGE: URL_PAGE,
     PRSE: URL_PRSE,
-    NPRS: URL_NPRS,
+    NPRS: URL_NPRS
 }
 
 // userland router metadata constants
-export const DOM_NODE = "NODE"
-export const DOM_BODY = "BODY"
-export const DOM_HEAD = "HEAD"
+export const DOM_NODE = 'NODE'
+export const DOM_BODY = 'BODY'
+export const DOM_HEAD = 'HEAD'
 
 // public
 export const DOM = {
     NODE: DOM_NODE,
     BODY: DOM_BODY,
-    HEAD: DOM_HEAD,
+    HEAD: DOM_HEAD
 }
 
 // set$$tate constants
-export const STATE_PATH = "PATH"
-export const STATE_DATA = "DATA"
+export const STATE_PATH = 'PATH'
+export const STATE_DATA = 'DATA'
 
 // public
 export const STATE = {
     PATH: STATE_PATH,
-    DATA: STATE_DATA,
+    DATA: STATE_DATA
 }
 
 // state setting Command constants
-export const CMD_SUB$ = "sub$"
-export const CMD_ARGS = "args"
-export const CMD_RESO = "reso"
-export const CMD_ERRO = "erro"
-export const CMD_WORK = "work"
-export const CMD_SRC$ = "src$"
+export const CMD_SUB$ = 'sub$'
+export const CMD_ARGS = 'args'
+export const CMD_RESO = 'reso'
+export const CMD_ERRO = 'erro'
+export const CMD_WORK = 'work'
+export const CMD_SRC$ = 'src$'
 
 // public
 export const CMD = {
@@ -66,18 +66,18 @@ export const CMD = {
     RESO: CMD_RESO,
     ERRO: CMD_ERRO,
     WORK: CMD_WORK,
-    SRC$: CMD_SRC$,
+    SRC$: CMD_SRC$
 }
 
 // boot config constants
-export const CFG_RUN$ = "run"
-export const CFG_ROOT = "root"
-export const CFG_VIEW = "view"
-export const CFG_KICK = "kick"
-export const CFG_STOR = "store"
-export const CFG_DRFT = "draft"
-export const CFG_LOG$ = "trace"
-export const CFG_RUTR = "router"
+export const CFG_RUN$ = 'run'
+export const CFG_ROOT = 'root'
+export const CFG_VIEW = 'view'
+export const CFG_KICK = 'kick'
+export const CFG_STOR = 'store'
+export const CFG_DRFT = 'draft'
+export const CFG_LOG$ = 'trace'
+export const CFG_RUTR = 'router'
 
 // public
 export const CFG = {
@@ -88,30 +88,30 @@ export const CFG = {
     DRFT: CFG_DRFT,
     LOG$: CFG_LOG$,
     RUTR: CFG_RUTR,
-    KICK: CFG_KICK,
+    KICK: CFG_KICK
 }
 
 // ROUTER
-export const ROUTER_PREP = "prep"
-export const ROUTER_POST = "post"
-export const ROUTER_PRFX = "prefix"
+export const ROUTER_PREP = 'prep'
+export const ROUTER_POST = 'post'
+export const ROUTER_PRFX = 'prefix'
 
 // public
 export const ROUTER = {
     PREP: ROUTER_PREP,
     POST: ROUTER_POST,
     PRFX: ROUTER_PRFX,
-    RUTR: CFG_RUTR,
+    RUTR: CFG_RUTR
 }
 
 // Global state keys/constants
 // underscores to prevent conflict with userland Commands
-// TODO: test
-export const $$_PATH = "_ROUTE_PATH"
-export const $$_LOAD = "_ROUTE_LOADING"
-export const $$_VIEW = "_PAGE_TEMPLATE"
-export const $$_ROOT = "_DOM_ROOT"
-export const $$_CMDS = "_COMMANDS"
+// TODO
+export const $$_PATH = '_ROUTE_PATH'
+export const $$_LOAD = '_ROUTE_LOADING'
+export const $$_VIEW = '_PAGE_TEMPLATE'
+export const $$_ROOT = '_DOM_ROOT'
+export const $$_CMDS = '_COMMANDS'
 
 // public
 export const $$: Object = {
@@ -119,7 +119,7 @@ export const $$: Object = {
     LOAD: $$_LOAD,
     VIEW: $$_VIEW,
     ROOT: $$_ROOT,
-    CMDS: $$_CMDS,
+    CMDS: $$_CMDS
 }
 
 // home page / defaults to empty path
@@ -128,5 +128,5 @@ export const $$_DEFAULT = {
     [$$_LOAD]: true,
     [$$_VIEW]: null,
     [$$_ROOT]: null,
-    [$$_CMDS]: {},
+    [$$_CMDS]: {}
 }
