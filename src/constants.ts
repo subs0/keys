5cea9b65807d4d3c7b7e93b1b3894236ccf4b035
Logan Powell <loganp@tepper.cmu.edu>
2021-02-05 06:13:03 -0500

    switching to subtree directory and running updates

--- src/constants.ts
+++ src/constants.ts
@@ -105,7 +105,7 @@ export const ROUTER = {
 }
 
 // Global state keys/constants
-// underscores to prevent conflict with userland Commands
+// underscored to prevent conflict with userland Commands
 export const $$_PATH = "_ROUTE_PATH"
 export const $$_LOAD = "_ROUTE_LOADING"
 export const $$_VIEW = "_PAGE_TEMPLATE"
