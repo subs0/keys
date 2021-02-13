d37d51d8e6bbcd1ab2e374b72c2751bbb8bad38e
Logan Powell <loganp@tepper.cmu.edu>
2021-02-13 09:25:50 -0500

    switching to subtree directory and running updates

--- src/constants.ts
+++ src/constants.ts
@@ -62,7 +62,7 @@ export const STATE = {
 }
 
 // Primary Userland Command Properties
-// PRIORITY: NEVER CHANGE THESE ⚠
+// PRIORITY: NEVER CHANGE THESE 💢
 export const CMD_SUB$ = "sub$"
 export const CMD_ARGS = "args"
 export const CMD_RESO = "reso"
