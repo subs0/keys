51a353f1105f428ce193187f226f36e9e61816b7
Logan Powell <loganp@tepper.cmu.edu>
2021-08-18 12:15:14 -0400

    switching to subtree directory and running updates

--- lib/api.d.ts
+++ lib/api.d.ts
@@ -1,65 +1,65 @@
 import { ISubscribable, ISubscriber } from "@thi.ng/rstream/api";
-import * as C from "./constants";
+import { CMD_ARGS, CMD_SRC$, CMD_ERRO, CMD_RESO, CMD_SUB$, CMD_WORK, URL_FULL, URL_PATH, URL_DOMN, URL_SUBD, URL_QERY, URL_HASH, DOM_NODE, DOM_BODY, DOM_HEAD, HD_TITL, HD_DESC, HD_IMGU, HD_IMGW, HD_IMGH, HD_ICON, HD_TYPE, URL_DATA, URL_PAGE, RTR_PREP, RTR_PRFX, RTR_POST, CFG_RUTR, $$_PATH, $$_LOAD, $$_VIEW, $$_ROOT } from "./constants";
 import { PubSub } from "@thi.ng/rstream";
 export declare type Accumulator = Record<string, unknown>;
 export declare type ICommandObject = {
-    [C.CMD_ARGS]: any;
-    [C.CMD_SUB$]?: string;
-    [C.CMD_RESO]?: (acc: Accumulator, res: any) => any;
-    [C.CMD_ERRO]?: (acc: Accumulator, err: Error, out$: PubSub<unknown, unknown, any>) => any;
+    [CMD_ARGS]: any;
+    [CMD_SUB$]?: string;
+    [CMD_RESO]?: (acc: Accumulator, res: any) => any;
+    [CMD_ERRO]?: (acc: Accumulator, err: Error, out$: PubSub<unknown, unknown, any>) => any;
 };
 export declare type ICommand = ICommandObject & {
-    [C.CMD_WORK]: (args: any) => any;
-    [C.CMD_SRC$]?: ISubscribable<any> | ISubscriber<any>;
+    [CMD_WORK]: (args: any) => any;
+    [CMD_SRC$]?: ISubscribable<any> | ISubscriber<any>;
 };
 export declare type Command = ICommandObject | HOTask;
 export declare type HOTask = (acc: Accumulator) => Task;
 export declare type Task = Command[];
 export declare type Component = (data: any) => any;
 export declare type ParsedURL = {
-    [C.URL_FULL]: string;
-    [C.URL_PATH]: string[];
-    [C.URL_DOMN]: string[];
-    [C.URL_SUBD]: string[];
-    [C.URL_QERY]: Record<string, unknown>;
-    [C.URL_HASH]: string;
+    [URL_FULL]: string;
+    [URL_PATH]: string[];
+    [URL_DOMN]: string[];
+    [URL_SUBD]: string[];
+    [URL_QERY]: Record<string, unknown>;
+    [URL_HASH]: string;
 };
 export declare type TargetDOM = {
-    [C.DOM_NODE]?: HTMLElement;
-    [C.DOM_BODY]?: Record<string, unknown>;
-    [C.DOM_HEAD]?: Record<string, unknown>;
+    [DOM_NODE]?: HTMLElement;
+    [DOM_BODY]?: Record<string, unknown>;
+    [DOM_HEAD]?: Record<string, unknown>;
 };
 export declare type HeadData = {
-    [C.HD_TITL]?: string;
-    [C.HD_DESC]?: string;
-    [C.HD_IMGU]?: string;
-    [C.HD_IMGW]?: string | number;
-    [C.HD_IMGH]?: string | number;
-    [C.HD_ICON]?: string;
-    [C.HD_TYPE]?: string;
+    [HD_TITL]?: string;
+    [HD_DESC]?: string;
+    [HD_IMGU]?: string;
+    [HD_IMGW]?: string | number;
+    [HD_IMGH]?: string | number;
+    [HD_ICON]?: string;
+    [HD_TYPE]?: string;
 };
 export declare type RouterHeadBodyData = {
-    [C.DOM_HEAD]: HeadData;
-    [C.DOM_BODY]: any;
+    [DOM_HEAD]: HeadData;
+    [DOM_BODY]: any;
 };
 export declare type RouterOutput = {
-    [C.URL_DATA]: RouterHeadBodyData | any;
-    [C.URL_PAGE]: Component;
+    [URL_DATA]: RouterHeadBodyData | any;
+    [URL_PAGE]: Component;
 };
 export declare type Router = (path: string) => RouterOutput;
 export declare type RouterInput = {
-    [C.URL_FULL]: string;
-    [C.DOM_NODE]?: HTMLElement | Window;
+    [URL_FULL]: string;
+    [DOM_NODE]?: HTMLElement | Window;
 };
 export declare type RouterCFG = {
-    [C.RTR_PREP]: Command | Task;
-    [C.RTR_PRFX]: string;
-    [C.RTR_POST]: Command | Task;
-    [C.CFG_RUTR]: Router;
+    [RTR_PREP]: Command | Task;
+    [RTR_PRFX]: string;
+    [RTR_POST]: Command | Task;
+    [CFG_RUTR]: Router;
 };
 export declare type DefaultDraft = {
-    [C.$$_PATH]: string[];
-    [C.$$_LOAD]: boolean;
-    [C.$$_VIEW]: Component;
-    [C.$$_ROOT]: HTMLElement;
+    [$$_PATH]: string[];
+    [$$_LOAD]: boolean;
+    [$$_VIEW]: Component;
+    [$$_ROOT]: HTMLElement;
 };
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,39 @@
 import { ISubscribable, ISubscriber } from "@thi.ng/rstream/api"
 import { IAtom } from "@thi.ng/atom/api"
-import * as C from "./constants"
+import {
+    CMD_ARGS,
+    CMD_SRC$,
+    CMD_ERRO,
+    CMD_RESO,
+    CMD_SUB$,
+    CMD_WORK,
+    URL_FULL,
+    URL_PATH,
+    URL_DOMN,
+    URL_SUBD,
+    URL_QERY,
+    URL_HASH,
+    DOM_NODE,
+    DOM_BODY,
+    DOM_HEAD,
+    HD_TITL,
+    HD_DESC,
+    HD_IMGU,
+    HD_IMGW,
+    HD_IMGH,
+    HD_ICON,
+    HD_TYPE,
+    URL_DATA,
+    URL_PAGE,
+    RTR_PREP,
+    RTR_PRFX,
+    RTR_POST,
+    CFG_RUTR,
+    $$_PATH,
+    $$_LOAD,
+    $$_VIEW,
+    $$_ROOT,
+} from "./constants"
 import { PubSub } from "@thi.ng/rstream"
 
 export type Accumulator = Record<string, unknown>
@@ -11,15 +44,15 @@ export type Accumulator = Record<string, unknown>
  * The only required property is `args`
  */
 export type ICommandObject = {
-    [C.CMD_ARGS]: any
-    [C.CMD_SUB$]?: string
-    [C.CMD_RESO]?: (acc: Accumulator, res: any) => any
-    [C.CMD_ERRO]?: (acc: Accumulator, err: Error, out$: PubSub<unknown, unknown, any>) => any
+    [CMD_ARGS]: any
+    [CMD_SUB$]?: string
+    [CMD_RESO]?: (acc: Accumulator, res: any) => any
+    [CMD_ERRO]?: (acc: Accumulator, err: Error, out$: PubSub<unknown, unknown, any>) => any
 }
 
 export type ICommand = ICommandObject & {
-    [C.CMD_WORK]: (args: any) => any
-    [C.CMD_SRC$]?: ISubscribable<any> | ISubscriber<any>
+    [CMD_WORK]: (args: any) => any
+    [CMD_SRC$]?: ISubscribable<any> | ISubscriber<any>
 }
 
 export type Command = ICommandObject | HOTask
@@ -31,60 +64,60 @@ export type Task = Command[]
 export type Component = (data: any) => any
 
 export type ParsedURL = {
-    [C.URL_FULL]: string
-    [C.URL_PATH]: string[]
-    [C.URL_DOMN]: string[]
-    [C.URL_SUBD]: string[]
-    [C.URL_QERY]: Record<string, unknown>
-    [C.URL_HASH]: string
+    [URL_FULL]: string
+    [URL_PATH]: string[]
+    [URL_DOMN]: string[]
+    [URL_SUBD]: string[]
+    [URL_QERY]: Record<string, unknown>
+    [URL_HASH]: string
 }
 
 /**
  * Provides targets for data injection from DOM router
  */
 export type TargetDOM = {
-    [C.DOM_NODE]?: HTMLElement
-    [C.DOM_BODY]?: Record<string, unknown>
-    [C.DOM_HEAD]?: Record<string, unknown>
+    [DOM_NODE]?: HTMLElement
+    [DOM_BODY]?: Record<string, unknown>
+    [DOM_HEAD]?: Record<string, unknown>
 }
 
 export type HeadData = {
-    [C.HD_TITL]?: string
-    [C.HD_DESC]?: string
-    [C.HD_IMGU]?: string
-    [C.HD_IMGW]?: string | number
-    [C.HD_IMGH]?: string | number
-    [C.HD_ICON]?: string
-    [C.HD_TYPE]?: string
+    [HD_TITL]?: string
+    [HD_DESC]?: string
+    [HD_IMGU]?: string
+    [HD_IMGW]?: string | number
+    [HD_IMGH]?: string | number
+    [HD_ICON]?: string
+    [HD_TYPE]?: string
 }
 
 export type RouterHeadBodyData = {
-    [C.DOM_HEAD]: HeadData
-    [C.DOM_BODY]: any
+    [DOM_HEAD]: HeadData
+    [DOM_BODY]: any
 }
 
 export type RouterOutput = {
-    [C.URL_DATA]: RouterHeadBodyData | any
-    [C.URL_PAGE]: Component
+    [URL_DATA]: RouterHeadBodyData | any
+    [URL_PAGE]: Component
 }
 
 export type Router = (path: string) => RouterOutput
 
 export type RouterInput = {
-    [C.URL_FULL]: string
-    [C.DOM_NODE]?: HTMLElement | Window
+    [URL_FULL]: string
+    [DOM_NODE]?: HTMLElement | Window
 }
 
 export type RouterCFG = {
-    [C.RTR_PREP]: Command | Task
-    [C.RTR_PRFX]: string
-    [C.RTR_POST]: Command | Task
-    [C.CFG_RUTR]: Router
+    [RTR_PREP]: Command | Task
+    [RTR_PRFX]: string
+    [RTR_POST]: Command | Task
+    [CFG_RUTR]: Router
 }
 
 export type DefaultDraft = {
-    [C.$$_PATH]: string[]
-    [C.$$_LOAD]: boolean
-    [C.$$_VIEW]: Component
-    [C.$$_ROOT]: HTMLElement
+    [$$_PATH]: string[]
+    [$$_LOAD]: boolean
+    [$$_VIEW]: Component
+    [$$_ROOT]: HTMLElement
 }
