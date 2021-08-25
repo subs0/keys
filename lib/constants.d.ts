983e1dcb7f4c1171d0c6c770d5bec14c7309d784
Logan Powell <loganp@tepper.cmu.edu>
2021-08-25 11:48:43 -0400

    switching to subtree directory and running updates

---
+++ lib/constants.d.ts
@@ -0,0 +1,104 @@
+export declare const URL_PAGE = "page";
+export declare const URL_DATA = "data";
+export declare const URL_FULL = "fURL";
+export declare const URL_PATH = "path";
+export declare const URL_DOMN = "domn";
+export declare const URL_SUBD = "subd";
+export declare const URL_QERY = "qery";
+export declare const URL_HASH = "hash";
+export declare const URL_PRSE = "unfurl";
+export declare const URL_NPRS = "urlify";
+export declare const URL: {
+    FULL: string;
+    PATH: string;
+    DATA: string;
+    DOMN: string;
+    SUBD: string;
+    QERY: string;
+    HASH: string;
+    PAGE: string;
+    PRSE: string;
+    NPRS: string;
+};
+export declare const DOM_NODE = "node";
+export declare const DOM_BODY = "body";
+export declare const DOM_HEAD = "head";
+export declare const DOM: {
+    NODE: string;
+    BODY: string;
+    HEAD: string;
+};
+export declare const NAV: {
+    FULL: string;
+    NODE: string;
+};
+export declare const HD_TITL = "title";
+export declare const HD_ICON = "favicon";
+export declare const HD_DESC = "og_description";
+export declare const HD_IMGU = "og_image";
+export declare const HD_IMGW = "og_image_width";
+export declare const HD_IMGH = "og_image_height";
+export declare const HD_TYPE = "og_type";
+export declare const HEAD: {
+    TITL: string;
+    ICON: string;
+    DESC: string;
+    IMGU: string;
+    IMGW: string;
+    IMGH: string;
+    TYPE: string;
+};
+export declare const STATE_PATH = "path";
+export declare const STATE_DATA = "data";
+export declare const STATE: {
+    PATH: string;
+    DATA: string;
+};
+export declare const CMD_SUB$ = "sub$";
+export declare const CMD_ARGS = "args";
+export declare const CMD_RESO = "reso";
+export declare const CMD_ERRO = "erro";
+export declare const CMD_WORK = "work";
+export declare const CMD_SRC$ = "src$";
+export declare const CMD: {
+    SUB$: string;
+    ARGS: string;
+    RESO: string;
+    ERRO: string;
+    WORK: string;
+    SRC$: string;
+};
+export declare const CFG_RUN$ = "run";
+export declare const CFG_STOR = "state";
+export declare const CFG_LOG$ = "log$";
+export declare const CFG_RUTR = "router";
+export declare const CTX: {
+    STOR: string;
+    LOG$: string;
+    RUN$: string;
+};
+export declare const RTR_PREP = "preroute";
+export declare const RTR_POST = "postroute";
+export declare const RTR_PRFX = "ignore_prefix";
+export declare const RTR: {
+    PREP: string;
+    POST: string;
+    PRFX: string;
+    RUTR: string;
+};
+export declare const $$_PATH = "$$_PATH";
+export declare const $$_LOAD = "$$_LOAD";
+export declare const $$_VIEW = "$$_VIEW";
+export declare const $$_ROOT = "$$_ROOT";
+export declare const $$: {
+    PATH: string;
+    LOAD: string;
+    VIEW: string;
+    ROOT: string;
+};
+export declare const $$_DEFAULT: {
+    $$_PATH: any[];
+    $$_LOAD: boolean;
+    $$_VIEW: any;
+    $$_ROOT: any;
+};
